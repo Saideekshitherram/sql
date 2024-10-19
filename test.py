9c29a0490f2e22af44c83944e93be9a51a9828e2
Saideekshitherram <156347661+Saideekshitherram@users.noreply.github.com>
2024-10-19 13:41:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from mysql.connector import Error
 
 def test_db_connection():
     try:
-        # Replace these values with your database credentials
+        # Database credentials
         connection = mysql.connector.connect(
-            host="localhost",         # e.g., "localhost"
-            user="root",     # e.g., "root"
-            password="", # e.g., "password"
-            database="sql"  # e.g., "test_db" (optional if connecting to a specific database)
+            host="localhost",        
+            user="root",     
+            password="", 
+            database="sql"  
         )
         
         if connection.is_connected():
